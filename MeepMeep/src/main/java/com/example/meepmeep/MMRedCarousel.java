ed42343812a345069361a4389a2fdd58cae4cec1
Saber <48610047+garagebot15146@users.noreply.github.com>
2022-02-27 18:05:54 -0800

    Started red carousel

--- MeepMeep/src/main/java/com/example/meepmeep/MMRedCarousel.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MMRedCarousel.java
@@ -13,18 +13,20 @@ public class MMRedCarousel {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(45, 45, Math.toRadians(300), Math.toRadians(300), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34, -64, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(-19, -42.7, Math.toRadians(70)))
+                                drive.trajectorySequenceBuilder(new Pose2d(-34, -64, Math.toRadians(90)))
+                                        .lineToLinearHeading(new Pose2d(-19, -42.7, Math.toRadians(70)))
 //                                .UNSTABLE_addTemporalMarkerOffset(0, () ->  liftUp(11, 3))
 //                                .UNSTABLE_addTemporalMarkerOffset(1, () -> deposit())
 //                                .UNSTABLE_addTemporalMarkerOffset(2, () -> liftDown())
-                                .waitSeconds(3)
-                                .lineToLinearHeading(new Pose2d(-64, -49, Math.toRadians(0)))
-                                .lineToLinearHeading(new Pose2d(-64, -55, Math.toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(-64, -49, Math.toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(-64, -55, Math.toRadians(0)))
 //                                .UNSTABLE_addTemporalMarkerOffset(0.3, () -> duckRed(3))
-                                .waitSeconds(3)
-                                .lineToLinearHeading(new Pose2d(-61, -36, Math.toRadians(0)))
-                                .build()
+                                        .lineToLinearHeading(new Pose2d(-56, -49, Math.toRadians(90)))
+                                        //                                .UNSTABLE_addTemporalMarkerOffset(0, () -> intake())
+                                        .lineToLinearHeading(new Pose2d(-56, -64, Math.toRadians(90)))
+                                        .lineToLinearHeading(new Pose2d(-19, -42.7, Math.toRadians(70)))
+                                        .lineToLinearHeading(new Pose2d(-64, -40, Math.toRadians(0)))
+                                        .build()
                 );
 
 
